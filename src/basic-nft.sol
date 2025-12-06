959d4451f99b3bffec62976b1385c8f733d0dda2
himxa <himxa0x@gmail.com>
2025-12-06 20:21:46 +0100

    set up the core feature of the basic NFT

---
+++ src/basic-nft.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//each nft have unique token id
+
+contract BasicNft is ERC721{
+uint256 private s_tokenCounter;
+
+mapping(uint256=> string) private s_tokenURI;
+
+
+constructor()ERC721("Dogie","DOG"){
+s_tokenCounter = 0;
+
+}
+
+function mintNft(string memory tokenURI) external {
+  s_tokenURI[s_tokenCounter] = tokenURI;
+  _safeMint(msg.sender, s_tokenCounter);
+  s_tokenCounter++;
+}
+
+//
+
+function tokenURI(uint256 tokenId)public  view  override returns(string memory){
+
+
+ return s_tokenURI[tokenId];
+}
+
+}
